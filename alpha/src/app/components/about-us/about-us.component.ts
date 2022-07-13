2f740e5858f9635c1307c96b33be53b7773f3056
Joshi <joshik@opentext.com>
2022-07-13 21:20:03 +0530

    About Us implementation !!!

--- alpha/src/app/components/about-us/about-us.component.ts
+++ alpha/src/app/components/about-us/about-us.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class AboutUsComponent implements OnInit {
 
   constructor() { }
-
+  activeService = "it-training";
   ngOnInit() {
   }
 
