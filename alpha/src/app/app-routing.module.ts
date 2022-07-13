f5d3eecfdf99f9308af5423d6a1f029257ee6010
Joshi <joshik@opentext.com>
2022-07-13 13:50:02 +0530

    Implementation of HashLocation strategy !!!

--- alpha/src/app/app-routing.module.ts
+++ alpha/src/app/app-routing.module.ts
@@ -32,10 +32,15 @@ const routes: Routes = [
     path : "contact",
     component: ContactComponent
   },
+  {
+    path: '',
+    redirectTo: "home",
+    pathMatch: "full",
+  }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash : true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- alpha/src/app/app.component.ts
+++ alpha/src/app/app.component.ts
@@ -1,9 +1,13 @@
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
+  ]
 })
 export class AppComponent {
   title = 'alpha';
